e2572bf031ec36bff72cf8a7090e7e3c39d521ee
aulia rahman <105280611+skimura-soft@users.noreply.github.com>
2022-05-10 18:34:09 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,533 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Kalteng Cyber Xploit</title>
+    <meta name="og:title" content="Kami ini adalah kalteng cyber xploit untuk melindungi serangan serangan ini"/>
+    <meta name="og:description" content="jangan lupa melihat website kami ini nanti ada update update lainnya"/>
+    <!-- ICON -->
+    <link href="https://kaltengcyberhosting.xyz/img/icon.jpg" rel="icon" />
+    <!-- Boxiocns CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="og:author" content="Rahman Hosting"/>
+     <meta name="copyright" content="By Kalteng Cyber Hosting"/>
+     <!-- Meta Open Graph -->
+<meta content='article' property='og:type'/>
+<meta content='id_ID' property='og:locale'/>
+<meta content='en_US' property='og:locale:alternate'/>
+<meta content='en_GB' property='og:locale:alternate'/>
+<meta content='summary_large_image' name='twitter:card'/>
+<!-- Meta Robots Search -->
+<meta content='all-language' http-equiv='Content-Language'/>
+<meta content='Indonesia' name='geo.placename'/>
+<meta content='id' name='geo.country'/>
+<meta content='ID-BT' name='geo.region'/>
+<meta content='id' name='language'/>
+<meta content='global' name='target'/>
+<meta content='global' name='distribution'/>
+<meta content='general' name='rating'/>
+<meta content='true' name='MSSmartTagsPreventParsing'/>
+<meta content='index, follow' name='googlebot'/>
+<meta content='follow, all' name='Googlebot-Image'/>
+<meta content='follow, all' name='msnbot'/>
+<meta content='follow, all' name='Slurp'/>
+<meta content='follow, all' name='ZyBorg'/>
+<meta content='follow, all' name='Scooter'/>
+<meta content='all' name='spiders'/>
+<meta content='all' name='WEBCRAWLERS'/>
+<meta content='aeiwi, alexa, alltheWeb, altavista, aol netfind, anzwers, canada, directhit, euroseek, excite, overture, go, google, hotbot. infomak, kanoodle, lycos, mastersite, national directory, northern light, searchit, simplesearch, Websmostlinked, webtop, what-u-seek, aol, yahoo, webcrawler, infoseek, excite, magellan, looksmart, bing, cnet, googlebot' name='search engines'/>
+<!-- Owner Sife Verification -->
+<!--<meta content='xxxx_xxxxx-xxxxxxx' name='majestic-site-verification'/>-->
+<meta name="google-site-verification" content="eAS9cETVEEDoY0x31GvDn3vtg6N8_9KtMkFCg3LsP8Q" />
+<meta name="yandex-verification" content="6d31e60810297b43" />
+<!-- Meta Title -->
+<meta content='kaltengcyberhosting.xyz' property='og:title'/>
+<meta content='kaltengcyberhosting.xyz' property='og:image:alt'/>
+<meta content='kaltengcyberhosting.xyz' name='twitter:title'/>
+<meta content='kaltengcyberhosting.xyz' name='twitter:image:alt'/>
+<meta content='kaltengcyberhosting.xyz' property='og:site_name'/>
+<!-- Meta Description -->
+<meta content='Media Sosial, Tekno, Berita, Game, Tutorial, Blogger, Adsense, SEO, Free Fire, Mobile Legends, Script, Viral, Trending, Tiktok, Youtube' name='description'/>
+<meta content='Media Sosial, Tekno, Berita, Game, Tutorial, Blogger, Adsense, SEO, Free Fire, Mobile Legends, Script, Viral, Trending, Tiktok, Youtube' property='og:description'/>
+<meta content='Media Sosial, Tekno, Berita, Game, Tutorial, Blogger, Adsense, SEO, Free Fire, Mobile Legends, Script, Viral, Trending, Tiktok, Youtube' name='twitter:description'/>
+<!-- Meta Keywords -->
+<meta content='kaltengcyberhosting.xyz, Media Sosial, Tekno, Berita, Game, Tutorial, Blogger, Adsense, SEO, Free Fire, Mobile Legends, Script, Viral, Trending, Tiktok, Youtube ' name='keywords'/>
+<meta content='kaltengcyberhosting.xyz' property='article:tag'/>
+<!-- Meta Link -->
+<!--<b:if cond='data:blog.isMobileRequest == &quot;false&quot;'> <link expr:href='data:blog.canonicalUrl' rel='canonical'/> <link expr:href='data:blog.url + &quot;?m=1&quot;' rel='amphtml'/> </b:if> <b:if cond='data:blog.isMobileRequest == &quot;true&quot;'> <link expr:href='data:blog.canonicalUrl' rel='canonical'/> </b:if> <link expr:href='data:blog.url' hreflang='x-default' rel='alternate'/> <meta expr:content='data:blog.canonicalUrl' property='og:url'/>-->
+<!--<link expr:href='data:blog.canonicalUrl' rel='canonical'/>-->
+<link href='https://kaltengcyberhosting.xyz/' rel='canonical'/>
+<link href='https://kaltengcyberhosting.xyz/' hreflang='x-default' rel='alternate'/>
+   </head>
+   <style>/* Google Fonts Import Link */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+.sidebar{
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 260px;
+  background: #11101d;
+  z-index: 100;
+  transition: all 0.5s ease;
+}
+.sidebar.close{
+  width: 78px;
+}
+.sidebar .logo-details{
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+.sidebar .logo-details i{
+  font-size: 30px;
+  color: #fff;
+  height: 50px;
+  min-width: 78px;
+  text-align: center;
+  line-height: 50px;
+}
+.sidebar .logo-details .logo_name{
+  font-size: 22px;
+  color: #fff;
+  font-weight: 600;
+  transition: 0.3s ease;
+  transition-delay: 0.1s;
+}
+.sidebar.close .logo-details .logo_name{
+  transition-delay: 0s;
+  opacity: 0;
+  pointer-events: none;
+}
+.sidebar .nav-links{
+  height: 100%;
+  padding: 30px 0 150px 0;
+  overflow: auto;
+}
+.sidebar.close .nav-links{
+  overflow: visible;
+}
+.sidebar .nav-links::-webkit-scrollbar{
+  display: none;
+}
+.sidebar .nav-links li{
+  position: relative;
+  list-style: none;
+  transition: all 0.4s ease;
+}
+.sidebar .nav-links li:hover{
+  background: #1d1b31;
+}
+.sidebar .nav-links li .iocn-link{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.sidebar.close .nav-links li .iocn-link{
+  display: block
+}
+.sidebar .nav-links li i{
+  height: 50px;
+  min-width: 78px;
+  text-align: center;
+  line-height: 50px;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.sidebar .nav-links li.showMenu i.arrow{
+  transform: rotate(-180deg);
+}
+.sidebar.close .nav-links i.arrow{
+  display: none;
+}
+.sidebar .nav-links li a{
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+.sidebar .nav-links li a .link_name{
+  font-size: 18px;
+  font-weight: 400;
+  color: #fff;
+  transition: all 0.4s ease;
+}
+.sidebar.close .nav-links li a .link_name{
+  opacity: 0;
+  pointer-events: none;
+}
+.sidebar .nav-links li .sub-menu{
+  padding: 6px 6px 14px 80px;
+  margin-top: -10px;
+  background: #1d1b31;
+  display: none;
+}
+.sidebar .nav-links li.showMenu .sub-menu{
+  display: block;
+}
+.sidebar .nav-links li .sub-menu a{
+  color: #fff;
+  font-size: 15px;
+  padding: 5px 0;
+  white-space: nowrap;
+  opacity: 0.6;
+  transition: all 0.3s ease;
+}
+.sidebar .nav-links li .sub-menu a:hover{
+  opacity: 1;
+}
+.sidebar.close .nav-links li .sub-menu{
+  position: absolute;
+  left: 100%;
+  top: -10px;
+  margin-top: 0;
+  padding: 10px 20px;
+  border-radius: 0 6px 6px 0;
+  opacity: 0;
+  display: block;
+  pointer-events: none;
+  transition: 0s;
+}
+.sidebar.close .nav-links li:hover .sub-menu{
+  top: 0;
+  opacity: 1;
+  pointer-events: auto;
+  transition: all 0.4s ease;
+}
+.sidebar .nav-links li .sub-menu .link_name{
+  display: none;
+}
+.sidebar.close .nav-links li .sub-menu .link_name{
+  font-size: 18px;
+  opacity: 1;
+  display: block;
+}
+.sidebar .nav-links li .sub-menu.blank{
+  opacity: 1;
+  pointer-events: auto;
+  padding: 3px 20px 6px 16px;
+  opacity: 0;
+  pointer-events: none;
+}
+.sidebar .nav-links li:hover .sub-menu.blank{
+  top: 50%;
+  transform: translateY(-50%);
+}
+.sidebar .profile-details{
+  position: fixed;
+  bottom: 0;
+  width: 260px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #1d1b31;
+  padding: 12px 0;
+  transition: all 0.5s ease;
+}
+.sidebar.close .profile-details{
+  background: none;
+}
+.sidebar.close .profile-details{
+  width: 78px;
+}
+.sidebar .profile-details .profile-content{
+  display: flex;
+  align-items: center;
+}
+.sidebar .profile-details img{
+  height: 52px;
+  width: 52px;
+  object-fit: cover;
+  border-radius: 16px;
+  margin: 0 14px 0 12px;
+  background: #1d1b31;
+  transition: all 0.5s ease;
+}
+.sidebar.close .profile-details img{
+  padding: 10px;
+}
+.sidebar .profile-details .profile_name,
+.sidebar .profile-details .job{
+  color: #fff;
+  font-size: 18px;
+  font-weight: 500;
+  white-space: nowrap;
+}
+.sidebar.close .profile-details i,
+.sidebar.close .profile-details .profile_name,
+.sidebar.close .profile-details .job{
+  display: none;
+}
+.sidebar .profile-details .job{
+  font-size: 12px;
+}
+.home-section{
+  position: relative;
+  background: #E4E9F7;
+  height: 100vh;
+  left: 260px;
+  width: calc(100% - 260px);
+  transition: all 0.5s ease;
+}
+.sidebar.close ~ .home-section{
+  left: 78px;
+  width: calc(100% - 78px);
+}
+.home-section .home-content{
+  height: 60px;
+  display: flex;
+  align-items: center;
+}
+.home-section .home-content .bx-menu,
+.home-section .home-content .text{
+  color: #11101d;
+  font-size: 35px;
+}
+.home-section .home-content .bx-menu{
+  margin: 0 15px;
+  cursor: pointer;
+}
+.home-section .home-content .text{
+  font-size: 26px;
+  font-weight: 600;
+}
+@media (max-width: 400px) {
+  .sidebar.close .nav-links li .sub-menu{
+    display: none;
+  }
+  .sidebar{
+    width: 78px;
+  }
+  .sidebar.close{
+    width: 0;
+  }
+  .home-section{
+    left: 78px;
+    width: calc(100% - 78px);
+    z-index: 100;
+  }
+  .sidebar.close ~ .home-section{
+    width: 100%;
+    left: 0;
+  }
+}
+</style>
+<body>
+  <div class="sidebar close">
+    <div class="logo-details">
+      <i class='bx bxl-kickstarter'></i>
+      <span class="logo_name">KCH LAB</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="index.php?id=home">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-wrench'></i>
+            <span class="link_name">Tools</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Tools</a></li>
+          <li><a href="tools/botnulis.html?id=1">Bot Nulis</a></li>
+          <li><a href="tools/whatsapp-nomor-tujuan.html?id=2">WhatsApp Nmor Tujuan</a></li>
+          <li><a href="tools/youtube.html?id=3">Youtube Downloader</a></li>
+          <li><a href="tools/whatsapp-read-more.html?id=4">WhatsApp Read More</a></li>
+          <li><a href="tools/image-encoder.html?id=5">Image Encoder</a></li>
+          <li><a href="tools/browsing.html?id=6">Browsing</a></li>
+          <li><a href="tools/google-drive.html?id=7">G-Drive Link Generator</a></li>
+          <li><a href="tools/suara-google.html?id=8">Text To Speech</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-wrench'></i>
+            <span class="link_name">Seo Tools</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Seo Tools</a></li>
+          <li><a href="tools/shell-scanner.php?id=12">Shell Scanner</a></li>
+          <li><a href="tools/html-parse.html?id=13">Html Parse</a></li>
+          <li><a href="tools/live/live-chat.html?livechat">Live Chat</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bxs-joystick'></i>
+            <span class="link_name">Games</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Games</a></li>
+          <li><a href="tools/2048tile.html?id=9">2048 Tile</a></li>
+          <li><a href="tools/puzzle.html?id=10">Puzzle</a></li>
+          <li><a href="tools/snake.html?id=11">Snake</a></li>
+        </ul>
+      </li>
+      <li>
+      <a href="https://deface.kaltengcyberhosting.xyz">
+      <i class='bx bxs-download'></i>
+      <span class="link_name">Deface Download</span>
+      </li>
+      <li>
+      <a href="https://shell.kaltengcyberhosting.xyz">
+      <i class='bx bxs-download'></i>
+      <span class="link_name">Shell Download</span>
+      </li>
+      <li>
+        <a href="donate/donasi.html">
+        <i class='bx bxs-wallet'></i>
+        <span class="link_name">Donasi</span>
+        </li>
+      <li>
+        <a href="settings.html?page=setting">
+          <i class='bx bx-cog' ></i>
+          <span class="link_name">Setting</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Setting</a></li>
+        </ul>
+      </li>
+      <li>
+    <div class="profile-details">
+      <div class="profile-content">
+        <img src="img/icon.jpg" alt="profileImg">
+      </div>
+      <div class="name-job">
+        <div class="profile_name">Aulia Rahman</div>
+        <div class="job">Developer Web</div>
+        <h1 style="font-size: 12px; color:white;" id="jam" class="job"></h1>
+      </div>
+      <i class='bx bx-log-out' ></i>
+    </div>
+  </li>
+</ul>
+  </div>
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu' ></i>
+      <span class="text">MENU</span>
+    </div>
+    <center><font color="black">
+    <h3>Welcome To Kalteng Cyber Xploit</h3><br>
+    <img src="img/icon.jpg" width="100">
+    <h3>By &copy; Aulia Rahman</h3><br><br>
+    <p>Skill Kami Ini</p><br>
+    <p>Program, Tutorial, Script Bucin, Script Termux, Bug Xss, Bug Hunter, Bug Sql, Bug Sqli, Defacer, Dll</p><br><br>
+    <h3>Rules Web Ini :<br>Jangan Rusak Web Ini<br>Gunakanlah Ilmu Program Mu Lebih Baik<br>Ada Yg Error Scriptnya Chat Admin<br><br>Terima Kasih Kpd :<br>Allah Swt<br>Aulia Rahman<br>Panther Lainnya</h3>
+    </font></center>
+    <script>
+    var x = prompt('apakah anda mau panther website saya kalau mau masuk silahkan ketik : "mau" dan klik ok? kalau gamau masuk panther website saya silahkan klik batal')
+        if (x == 'mau'){
+        alert('silahkan chat wa admin')
+      }
+      </script>
+    <br><br>
+    <div class="skill-bar">
+   <center><h2>Skill yang dimiliki</h2></center>
+   <label>HTML</label>
+   <div class="container">
+    <div class="skill html">90%</div>
+   </div>
+   <label>CSS</label>
+   <div class="container">
+    <div class="skill css">35%</div>
+   </div>
+   <label>Java Script</label>
+   <div class="container">
+    <div class="skill js">80%</div>
+   </div>
+   <label>PHP</label>
+   <div class="container">
+    <div class="skill php">20%</div>
+   </div>
+   <label>MySQL</label>
+   <div class="container">
+    <div class="skill mysql">30%</div>
+   </div>
+  </div>
+ </div>
+ <style>
+.container{
+   width: 100%;
+   background-color: #f2f2f2;
+   margin-bottom: 10px;
+   border-radius: 10px;
+  } 
+  .card{
+   width: 100%;
+   max-width: 400px;
+   border-radius: 5px;
+   border: 2px solid #ddd;
+  }
+  .skill-bar{
+   text-align: left;
+   padding: 10px;
+   font-size: 12px;
+  }
+  .skill{
+   color: #fff;
+   font-size: 12px;
+   text-align: center;
+   border-radius: 10px;
+  }
+  .html{width: 80%;background-color: #22eb72}
+  .css{width: 70%;background-color: #1E80FA}
+  .js{width: 50%;background-color: #DBE223}
+  .php{width: 40%;background-color: #E93232}
+  .mysql{width: 20%;background-color: #E93232}
+</style>
+  </section>
+  <script>
+  let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });
+  window.onload = function() { jam(); }
+
+ function jam() {
+  var e = document.getElementById('jam'),
+  d = new Date(), h, m, s;
+  h = d.getHours();
+  m = set(d.getMinutes());
+  s = set(d.getSeconds());
+
+  e.innerHTML = h +':'+ m +':'+ s;
+
+  setTimeout('jam()', 1000);
+ }
+
+ function set(e) {
+  e = e < 10 ? '0'+ e : e;
+  return e;
+ }
+  </script>
+
